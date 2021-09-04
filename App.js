3e5f7ee0fb100340c9786987df8d20c382a267ee
daneng1 <engeldb@gmail.com>
2021-09-03 18:01:00 -0700

    worked on button functionality

--- App.js
+++ App.js
@@ -2,19 +2,22 @@ import React, { useState } from 'react';
 import { Text, SafeAreaView, StyleSheet } from 'react-native';
 import { Focus } from './src/features/focus/Focus';
 import Constants from 'expo-constants';
+import { colors } from './src/utils/colors';
+import { Timer } from './src/features/timer/timer';
 
 export default function App() {
-  const [ focusSubject,setFocusSubject ] = useState(null);
+  const [focusSubject, setFocusSubject] = useState("gardening");
 
+  // Do work in Expo Snack and then download zip file
   return (
     <SafeAreaView style={styles.container}>
-      {focusSubject ? ( 
-        <Text>Here is where the timer will go</Text> 
-        ): ( 
-          <Focus addSubject={setFocusSubject}/> 
-        )}
-        <Text>{focusSubject}</Text>
-     </SafeAreaView>
+      {focusSubject ? (
+        <Timer focusSubject={focusSubject}/>
+      ) : (
+        <Focus addSubject={setFocusSubject} />
+      )}
+      <Text>{focusSubject}</Text>
+    </SafeAreaView>
   );
 }
 
@@ -24,6 +27,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     // padding: 100,
     alignItems: 'center',
-    backgroundColor: '#252250' ,
+    backgroundColor: colors.darkBlue,
   },
 });
--- src/features/timer/timer.js
+++ src/features/timer/timer.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { Countdown } from '../../components/countdown';
+import { RoundedButton } from '../../components/roundedButton'
 
 import { colors } from '../../utils/colors';
 import { fontSizes, spacing } from '../../utils/sizes';
 
 export const Timer = ({ focusSubject }) => {
+  const [isStarted, setIsStarted] = useState(false);
   return (
     <View style={styles.container}>
     <View style={styles.countdown}>
-      <Countdown/>
+      <Countdown isPaused={!isStarted}/>
     </View>
-      <View style ={{ padding: spacing.xl }}>
+      <View style={{ padding: spacing.xl }}>
         <Text style={styles.title}>Focusing on: </Text>
         <Text style={styles.task}>{focusSubject}</Text>
+      </View>
+      <View style={styles.buttonWrapper}>
+      {isStarted ? (
+      <RoundedButton title='Pause' size={100} onPress={() => setIsStarted(false)}/> ):(
+      <RoundedButton title='Start' size={100} onPress={() => setIsStarted(true)}/> 
+      )}
       </View>
     </View>
   );
@@ -37,5 +45,11 @@ const styles = StyleSheet.create({
     flex: 0.5,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+    buttonWrapper: {
+    flex: 0.25,
+    padding: spacing.md,
+    justifyContent: "center",
+    alignItems: "center",
   }
 });
